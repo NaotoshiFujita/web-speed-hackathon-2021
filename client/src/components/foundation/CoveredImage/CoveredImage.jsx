38502ef36949a51de41ebfb0f3b1c02245aeac5f
NaotoshiFujita <fujita.naotoshi@gmail.com>
2021-12-08 01:21:39 +0900

    Optimize lazy loading.

--- client/src/components/foundation/CoveredImage/CoveredImage.jsx
+++ client/src/components/foundation/CoveredImage/CoveredImage.jsx
@@ -7,20 +7,21 @@ import { fetchBinary } from '../../../utils/fetchers';
  * @typedef {object} Props
  * @property {string} alt
  * @property {string} src
+ * @property {boolean} lazy
  */
 
 /**
  * アスペクト比を維持したまま、要素のコンテンツボックス全体を埋めるように画像を拡大縮小します
  * @type {React.VFC<Props>}
  */
-const CoveredImage = ({ alt, src }) => {
+const CoveredImage = ({ alt, src, lazy = true }) => {
   return (
     <div className="relative w-full h-full overflow-hidden">
       <img
         alt={ alt }
         className={ 'relative w-full h-full object-cover' }
         src={ src }
-        loading="lazy"
+        loading={ lazy ? 'lazy' : '' }
       />
     </div>
   );
