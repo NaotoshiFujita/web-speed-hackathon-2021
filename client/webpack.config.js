ae58e3d8bda4e0530fbf78bef7857dab5e4b940c
NaotoshiFujita <fujita.naotoshi@gmail.com>
2021-12-08 20:08:35 +0900

    Install preact.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -95,6 +95,12 @@ const config = {
       fs: false,
       path: false,
     },
+    alias: {
+      'react'               : 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom'           : 'preact/compat',
+      'react/jsx-runtime'   : 'preact/jsx-runtime',
+    },
   },
   optimization: {
     minimize : __prod__,
