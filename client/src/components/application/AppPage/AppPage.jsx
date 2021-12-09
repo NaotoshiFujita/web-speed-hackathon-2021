cc413161517a8fa589429e46346932e1465519a6
NaotoshiFujita <fujita.naotoshi@gmail.com>
2021-12-09 17:27:37 +0900

    Optimizing LCP.

--- client/src/components/application/AppPage/AppPage.jsx
+++ client/src/components/application/AppPage/AppPage.jsx
@@ -14,8 +14,7 @@ import { Navigation } from '../Navigation';
 const AppPage = ({ activeUser, children, onRequestOpenAuthModal, onRequestOpenPostModal }) => {
   return (
     <div className="relative z-0 flex justify-center bg-gray-100">
-      <div className="flex flex-row-reverse max-w-full min-h-screen text-gray-800 bg-white">
-        <main className="relative z-0 flex-shrink pb-12 w-screen min-w-0 max-w-screen-sm lg:pb-0">{children}</main>
+      <div className="flex max-w-full min-h-screen text-gray-800 bg-white">
         <aside className="relative z-10">
           <Navigation
             activeUser={activeUser}
@@ -23,6 +22,7 @@ const AppPage = ({ activeUser, children, onRequestOpenAuthModal, onRequestOpenPo
             onRequestOpenPostModal={onRequestOpenPostModal}
           />
         </aside>
+        <main className="relative z-0 flex-shrink pb-12 w-screen min-w-0 max-w-screen-sm lg:pb-0">{children}</main>
       </div>
     </div>
   );
--- server/scripts/compress-image.js
+++ server/scripts/compress-image.js
@@ -2,14 +2,16 @@ const sharp = require( 'sharp' );
 const glob  = require( 'glob' );
 
 const GENERAL_IMAGE_WIDTH = 500;
-const PROFILE_IMAGE_WIDTH = 80;
+const PROFILE_IMAGE_WIDTH = 128;
 
 glob( '../public/images/**/*.jpg', {}, function ( err, files ) {
   files.forEach( path => {
+    const isProfile = path.includes( '/profiles/' );
+
     sharp( path )
-      .resize({ width: path.includes( '/profiles/' ) ? PROFILE_IMAGE_WIDTH : GENERAL_IMAGE_WIDTH })
+      .resize({ width: isProfile ? PROFILE_IMAGE_WIDTH : GENERAL_IMAGE_WIDTH })
       .avif( {
-        quality: 30,
+        quality: isProfile ? 20 : 30,
         speed: 0,
       } )
       .toFile( path.replace( '.jpg', '.avif' ) );
