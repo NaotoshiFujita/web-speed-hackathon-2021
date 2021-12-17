376941a2abeadd9a2a1d151f8895d9e038ab0dff
NaotoshiFujita <fujita.naotoshi@gmail.com>
2021-12-18 03:21:27 +0900

    Remove react-intersection-observer.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,41 +4,39 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppContainer } from './containers/AppContainer';
 import { loadableReady } from '@loadable/component';
 import { SWRConfig } from 'swr';
+import 'swr'; // Disable splitting the file.
 
 
-// window.addEventListener( 'load', () => {
-// setTimeout( () => {
-  const fallbackElm = document.getElementById( 'swr-fallback' );
-  let fallback = {};
+// Note that using a global value may worsen the TTI.
+const fallbackElm = document.getElementById( 'swr-fallback' );
+let fallback = {};
 
-  if ( fallbackElm ) {
-    try {
-      fallback = JSON.parse( fallbackElm.textContent );
-    } catch ( e ) {
-      console.error( e );
-    }
-
-    loadableReady( render );
-  } else {
-    render();
+if ( fallbackElm ) {
+  try {
+    fallback = JSON.parse( fallbackElm.textContent );
+  } catch ( e ) {
+    console.error( e );
   }
 
-  const config = {
-    shouldRetryOnError: false,
-    revalidateIfStale : false,
-    revalidateOnFocus : false,
-    fallback,
-  };
+  loadableReady( render );
+} else {
+  render();
+}
 
-  function render() {
-    ReactDOM.hydrate(
-      <BrowserRouter>
-        <SWRConfig value={ config }>
-          <AppContainer/>
-        </SWRConfig>
-      </BrowserRouter>,
-      document.getElementById( 'app' )
-    );
-  }
-// } );
-// } );
+const config = {
+  shouldRetryOnError: false,
+  revalidateIfStale : false,
+  revalidateOnFocus : false,
+  fallback,
+};
+
+function render() {
+  ReactDOM.hydrate(
+    <BrowserRouter>
+      <SWRConfig value={ config }>
+        <AppContainer/>
+      </SWRConfig>
+    </BrowserRouter>,
+    document.getElementById( 'app' )
+  );
+}
