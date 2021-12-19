3e2f158404a4837fabcb4dd61765046c3b9b4503
NaotoshiFujita <fujita.naotoshi@gmail.com>
2021-12-19 18:59:48 +0900

    Send the not found page when accessing the invalid path.

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,7 @@ import { apiRouter } from './routes/api';
 import { staticRouter } from './routes/static';
 import { compressionRouter } from './routes/compression';
 import { pageRouter } from './routes/page';
+import { render } from './ssr/render';
 
 
 const app = Express();
@@ -29,5 +30,15 @@ app.use( '/api/v1', apiRouter );
 app.use( compressionRouter );
 app.use( pageRouter );
 app.use( staticRouter );
+
+app.use( async ( req, res ) => {
+  const { fallback } = res.locals;
+
+  return res
+    .set( 'Content-Type', 'text/html; charset=UTF-8' )
+    .set( 'Cache-control', 'max-age=0, no-store' )
+    .status( 404 )
+    .send( await render( req.url, fallback ) );
+} );
 
 export { app };
--- server/src/routes/static.js
+++ server/src/routes/static.js
@@ -1,4 +1,4 @@
-import history from 'connect-history-api-fallback';
+// import history from 'connect-history-api-fallback';
 import Router from 'express-promise-router';
 import serveStatic from 'serve-static';
 import { CLIENT_DIST_PATH, PUBLIC_PATH, UPLOAD_PATH } from '../paths';
@@ -8,7 +8,7 @@ import { MONTH_IN_MILLISECONDS, YEAR_IN_MILLISECONDS } from '../constants/time';
 const router = Router();
 
 // SPA 対応のため、ファイルが存在しないときに index.html を返す
-router.use( history() );
+// router.use( history() );
 
 router.use(
   serveStatic( UPLOAD_PATH, {
